b013d8e6c3523022ea915f5b65e37b056c100076
João Bosco Dos Reis Becker <joao.jbosco@gmail.com>
2024-12-15 13:02:10 -0300

    Add Files about basic Linq.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleLinq {
+    internal static class Program {
+        private static void Main() {
+            
+            // ⚡ LINQ - Language Integrated Query ⚡
+            // https://docs.microsoft.com/en-us/dotnet/csharp/linq/ 
+
+            // Specify a data source
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            // Define the query expression
+            var result = numbers
+                .Where(x => x % 2 == 0)
+                .Select(x => x * 10);
+            foreach (var num in result) {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
